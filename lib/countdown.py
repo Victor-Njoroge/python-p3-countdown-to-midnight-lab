93f7e141065eb78a48aa48af4a3562cb6427b00a
Victor Njoroge <victornjoroge2020@gmail.com>
2023-08-25 15:20:53 +0300

    done

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,11 @@
 # your code goes here!
+def CountDown():
+    seconds=int(input("Enter the seconds Here: "))
+    i=seconds
+    while i >= 0:
+        if i >0 :
+            print(f"{i} SECOND(S) !")
+        else:
+            print("HAPPY NEW YEAR")
+        i-=1
+CountDown()
